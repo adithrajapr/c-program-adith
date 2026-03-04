729673df86389b7ff637e8ec0f9a4efbbe70b90d
adithrajapr <adithrajapr2008@gmail.com>
2026-03-04 14:07:48 +0530

    Added c program

--- codes.c
+++ codes.c
@@ -1,6 +1,34 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
   printf("Hello World!");
   return 0;
+}*/
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%f", &a);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%f", &b);
+
+    if(op == '+')
+        printf("Result = %.2f", a + b);
+    else if(op == '-')
+        printf("Result = %.2f", a - b);
+    else if(op == '*')
+        printf("Result = %.2f", a * b);
+    else if(op == '/')
+        printf("Result = %.2f", a / b);
+    else
+        printf("Invalid operator");
+
+    return 0;
 }
