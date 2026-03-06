0eddcda621d0f827a852a349dcf6c8b29b63e701
adithrajapr <adithrajapr2008@gmail.com>
2026-03-06 21:17:11 +0530

    added

--- codes.c
+++ codes.c
@@ -233,3 +233,23 @@ int main()
 
     return 0;
 }*/
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 10, temp;
+    int *p1, *p2;
+
+    p1 = &a;
+    p2 = &b;
+
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+
+    printf("After swap:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
